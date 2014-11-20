078e9cf04ebba1a694c2fad37c0758d0da585385
Martin Kojtal <c0170@rocketmail.com>
2014-11-20 13:47:48 +0000

    Tests: Serial asynchronous loopback addition
    To test write/read functionality

--- workspace_tools/tests.py
+++ workspace_tools/tests.py
@@ -907,6 +907,12 @@ TESTS = [
         "dependencies": [MBED_LIBRARIES, TEST_MBED_LIB, CPPUTEST_LIBRARY, TEST_MBED_TESTRUNNER_LIB],
         "automated": False,
     },
+    {
+        "id": "UT_5", "description": "Asynchronous Serial test - Loopback",
+        "source_dir": join(TEST_DIR, "mbed", "serial_asynch_loopback"),
+        "dependencies": [MBED_LIBRARIES, TEST_MBED_LIB, CPPUTEST_LIBRARY, TEST_MBED_TESTRUNNER_LIB],
+        "automated": False,
+    },
 
     # Tests used for target information purposes
     {
