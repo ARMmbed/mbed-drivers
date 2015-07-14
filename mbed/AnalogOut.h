fd7c6bb0deb3ee5b45609b07041f9bc2858966c8
sg- <sam.w.grove@gmail.com>
2015-07-14 04:14:45 -0500

    update API for AnalogOut

--- mbed/AnalogOut.h
+++ mbed/AnalogOut.h
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+ * Copyright (c) 2006-2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,33 +17,24 @@
 #define MBED_ANALOGOUT_H
 
 #include "platform.h"
-
-#if DEVICE_ANALOGOUT
-
 #include "analogout_api.h"
+
+// int main()
+// {
+//     AnalogOut dac(DA0);
+//     dac.enable();
+//     dac.write(buffer, samples, &onDone);
+//     while(!done) {
+//         sleep();
+//         if (0xff == (dac.read() & 0xff)) {
+//             led = !led;
+//         }
+//     }
+//     dac.disable();
+// }
 
 namespace mbed {
 
-/** An analog output, used for setting the voltage on a pin
- *
- * Example:
- * @code
- * // Make a sawtooth output
- *
- * #include "mbed.h"
- *
- * AnalogOut tri(p18);
- * int main() {
- *     while(1) {
- *         tri = tri + 0.01;
- *         wait_us(1);
- *         if(tri == 1) {
- *             tri = 0;
- *         }
- *     }
- * }
- * @endcode
- */
 class AnalogOut {
 
 public:
@@ -56,59 +47,51 @@ public:
         analogout_init(&_dac, pin);
     }
 
-    /** Set the output voltage, specified as a percentage (float)
-     *
-     *  @param value A floating-point value representing the output voltage,
-     *    specified as a percentage. The value should lie between
-     *    0.0f (representing 0v / 0%) and 1.0f (representing 3.3v / 100%).
-     *    Values outside this range will be saturated to 0.0f or 1.0f.
-     */
-    void write(float value) {
-        analogout_write(&_dac, value);
+    ~AnalogOut() {
+        disable();
+        analogout_deinit();
     }
 
-    /** Set the output voltage, represented as an unsigned short in the range [0x0, 0xFFFF]
-     *
-     *  @param value 16-bit unsigned short representing the output voltage,
-     *            normalised to a 16-bit value (0x0000 = 0v, 0xFFFF = 3.3v)
-     */
-    void write_u16(unsigned short value) {
-        analogout_write_u16(&_dac, value);
+    void enalbe() {
+        analogout_on(&_dac);
     }
 
-    /** Return the current output voltage setting, measured as a percentage (float)
-     *
-     *  @returns
-     *    A floating-point value representing the current voltage being output on the pin,
-     *    measured as a percentage. The returned value will lie between
-     *    0.0f (representing 0v / 0%) and 1.0f (representing 3.3v / 100%).
-     *
-     *  @note
-     *    This value may not match exactly the value set by a previous write().
-     */
-    float read() {
-        return analogout_read(&_dac);
+    void disable() {
+        analogout_off(&_dac);
     }
 
-#ifdef MBED_OPERATORS
-    /** An operator shorthand for write()
-     */
-    AnalogOut& operator= (float percent) {
-        write(percent);
+    template <typename T>
+    void write_block(T *buffer, uint32_t amt, FunctionPointer0 userHandler = NULL) {
+        if (NULL == userHandler) {
+            analogout_write_block(&_dac, buffer, amt, sizeof(buffer), defaultHandler);
+            while(!done);
+        } else {
+            analogin_write_block(&_dac, buffer, amt, sizeof(buffer), userHandler);
+        }
+    }
+
+    template <typename T>
+    T read(T &value) {
+        value = analogout_read(&_dac);
+        return value;
+    }
+
+    template <typename T>
+    AnalogOut& operator= (T value) {
+        write_block(value, 1, sizeof(T));
         return *this;
     }
 
     AnalogOut& operator= (AnalogOut& rhs) {
-        write(rhs.read());
+        uint32_t value = rhs.read()
+        write_block(value, 1, sizeof(value));
         return *this;
     }
 
-    /** An operator shorthand for read()
-     */
-    operator float() {
-        return read();
+    operator int() {
+        uint32_t tmp;
+        return read(tmp);
     }
-#endif
 
 protected:
     dac_t _dac;
