3548b4e45c46dcc2becc8fd9c45c7181e9882d44
Martin Kojtal <martin.kojtal@arm.com>
2015-09-04 15:07:31 +0100

    pinmap functin additions for functionality

--- source/pinmap_common.c
+++ source/pinmap_common.c
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+ * Copyright (c) 2006-2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,4 +66,24 @@ uint32_t pinmap_peripheral(PinName pin, const PinMap* map) {
     if ((uint32_t)NC == peripheral) // no mapping available
         error("pinmap not found for peripheral");
     return peripheral;
+}
+
+uint32_t pinmap_find_function(PinName pin, const PinMap* map) {
+    while (map->pin != NC) {
+        if (map->pin == pin)
+            return map->function;
+        map++;
+    }
+    return (uint32_t)NC;
+}
+
+uint32_t pinmap_function(PinName pin, const PinMap* map) {
+    uint32_t function = (uint32_t)NC;
+
+    if (pin == (PinName)NC)
+        return (uint32_t)NC;
+    function = pinmap_find_function(pin, map);
+    if ((uint32_t)NC == function) // no mapping available
+        error("pinmap not found for function");
+    return function;
 }
